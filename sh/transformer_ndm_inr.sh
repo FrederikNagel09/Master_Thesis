fa38309538d26e0382777f610d6cb5a464183def
Frederik12345678 <frederiknagel9@gmail.com>
2026-04-12 09:21:04 +0200

    add fixes to make trainign work

--- sh/transformer_ndm_inr.sh
+++ sh/transformer_ndm_inr.sh
@@ -1,47 +1,53 @@
 #!/bin/bash
-#BSUB -J ndm_inr_CNN-MLP_Static_ADD_SIREN15                   # Job name
+#BSUB -J ndm_transinr_mnist_v3_full                  # Job name
 #BSUB -q gpuv100                            # Queue to submit the job to
-#BSUB -W 150                              # Wall time limit (6 hours)
+#BSUB -W 550                              # Wall time limit (6 hours)
 #BSUB -n 4                                 # Request 4 cores
 #BSUB -R "rusage[mem=512MB]"                 # Request 1 GB of memory per core
 #BSUB -R "span[hosts=1]"                   # Request all cores on the same host
 #BSUB -gpu "num=1:mode=exclusive_process"  # Request 1 GPU in exclusive mode
-#BSUB -o src/outputs/ndm_inr_CNN-MLP_Static_ADD_SIREN15.out                        # Standard output redirection
-#BSUB -e src/outputs/ndm_inr_CNN-MLP_Static_ADD_SIREN15.err                        # Standard error redirection
-#BSUB -N                                   # send email when job finishes
-#BSUB -B                                   # Send email when job begins
+#BSUB -o src/outputs/ndm_transinr_mnist_v3_full.out                        # Standard output redirection
+#BSUB -e src/outputs/ndm_transinr_mnist_v3_full.err                        # Standard error redirection
+##BSUB -N                                   # send email when job finishes
+##BSUB -B                                   # Send email when job begins
 
 # Activate virtual environment
 source /zhome/66/4/156534/Master_Thesis/.venv/bin/activate
 
 # --- Phase 1+2+3: Training ---
 python /zhome/66/4/156534/Master_Thesis/main.py \
-    --run_name ndm_inr_CNN-MLP_Static_ADD_SIREN15 \
-    --model ndm_inr \
-    --ndm_variant static \
-    --encoder_variant cnn \
-    --predictor_variant mlp \
+    --run_name ndm_transinr_mnist_v3_full \
+    --model ndm_transinr \
     --dataset mnist \
-    --use_modulation True \
-    --epochs 200 \
+    --epochs 100 \
     --batch_size 128 \
-    --lr 5e-4 \
-    --weight_decay 1e-5 \
+    --lr 1e-4 \
+    --weight_decay 1e-4 \
     --grad_clip 1.0 \
     --log_every_n_steps 20 \
     --subset_frac 1.0 \
-    --use_scheduler \
-    --warmup_steps 10000 \
-    --peak_lr 5e-4 \
+    --warmup_steps 2000 \
+    --peak_lr 3e-4 \
     --T 1000 \
     --beta_1 1e-4 \
     --beta_T 2e-2 \
     --sigma_tilde 1.0 \
-    --inr_hidden_dim 32 \
-    --omega_0 15.0 \
-    --inr_layers 3 \
-    --cnn_base_ch 128 \
-    --cnn_n_blocks 3 \
-    --noise_hidden_dim 512 \
-    --noise_n_blocks 4 \
+    --inr_hidden_dim 64 \
+    --inr_layers 4 \
+    --trans_dim 256 \
+    --trans_n_head 8 \
+    --trans_head_dim 32 \
+    --trans_ff_dim 512 \
+    --trans_enc_depth 4 \
+    --trans_dec_depth 4 \
+    --trans_patch_size 4 \
+    --trans_n_groups 8 \
+    --trans_update_strategy scale \
+    --predictor_variant transformer \
+    --transformer_chunk_size 32 \
+    --transformer_d_model 256 \
+    --transformer_n_heads 8 \
+    --transformer_n_layers 6 \
+    --transformer_d_ff 512 \
+    --transformer_dropout 0.0 \
     --noise_t_embed 128
