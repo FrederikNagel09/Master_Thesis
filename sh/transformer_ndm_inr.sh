e818256b329fe86137b1fabf81475c26d95a5d3b
Frederik12345678 <frederiknagel9@gmail.com>
2026-04-22 09:16:35 +0200

    do sum and mean instead

--- sh/transformer_ndm_inr.sh
+++ sh/transformer_ndm_inr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#BSUB -J TransNdmInr_temporal_v1            # Job name
+#BSUB -J Static-Transformer-NDM-INR_Baseline_mean_sum            # Job name
 #BSUB -q gpuv100                           # Queue to submit the job to
-#BSUB -W 1200                              # Wall time limit (6 hours)
+#BSUB -W 300                             # Wall time limit (6 hours)
 #BSUB -n 4                                 # Request 4 cores
 #BSUB -R "rusage[mem=1GB]"                 # Request 1 GB of memory per core
 #BSUB -R "span[hosts=1]"                   # Request all cores on the same host
 #BSUB -gpu "num=1:mode=exclusive_process"  # Request 1 GPU in exclusive mode
-#BSUB -o src/outputs/TransNdmInr_temporal_v1.out                        # Standard output redirection
-#BSUB -e src/outputs/TransNdmInr_temporal_v1.err                        # Standard error redirection
+#BSUB -o src/outputs/Static-Transformer-NDM-INR_Baseline_mean_sum.out                        # Standard output redirection
+#BSUB -e src/outputs/Static-Transformer-NDM-INR_Baseline_mean_sum.err                        # Standard error redirection
 #BSUB -N                                   # send email when job finishes
 #BSUB -B                                   # Send email when job begins
 
@@ -16,11 +16,11 @@ source /zhome/66/4/156534/Master_Thesis/.venv/bin/activate
 
 # --- Phase 1+2+3: Training ---
 python /zhome/66/4/156534/Master_Thesis/main.py \
-    --run_name TransNdmInr_temporal_v1 \
-    --model ndm_temporal_transinr\
+    --run_name Static-Transformer-NDM-INR_Baseline_mean_sum \
+    --model ndm_static_transinr\
     --dataset mnist \
     --epochs 100 \
-    --batch_size 128 \
+    --batch_size 64 \
     --lr 1e-4 \
     --weight_decay 1e-5 \
     --grad_clip 1.0 \
@@ -34,22 +34,21 @@ python /zhome/66/4/156534/Master_Thesis/main.py \
     --sigma_tilde 1.0 \
     --inr_hidden_dim 64 \
     --inr_layers 5 \
-    --encoder_trans_dim 512 \
+    --encoder_trans_dim 128 \
     --encoder_trans_n_head 4 \
     --encoder_trans_head_dim 32 \
-    --encoder_trans_ff_dim 1024 \
-    --encoder_trans_enc_depth 6 \
-    --encoder_trans_dec_depth 6 \
+    --encoder_trans_ff_dim 512 \
+    --encoder_trans_enc_depth 3 \
+    --encoder_trans_dec_depth 3 \
     --encoder_trans_patch_size 4 \
     --encoder_trans_n_groups 64 \
-    --encoder_time_freq_dim 512 \
     --encoder_trans_update_strategy scale \
     --predictor_variant transformer \
-    --noise_predictor_dim 512 \
+    --noise_predictor_dim 128 \
     --noise_predictor_n_head 4 \
     --noise_predictor_head_dim 32 \
-    --noise_predictor_ff_dim 1024 \
-    --noise_predictor_depth 10 \
+    --noise_predictor_ff_dim 512 \
+    --noise_predictor_depth 5 \
     --noise_predictor_dropout 0.1 \
-    --noise_predictor_t_embed_dim 512
+    --noise_predictor_t_embed_dim 256
    
