2c744ff723da661c154ccba7eebb9a48dd572b73
Frederik12345678 <frederiknagel9@gmail.com>
2026-05-23 07:37:47 +0200

    add sh script for vae

---
+++ sh/vae-baseline-training.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#BSUB -J vae-mnist           # Job name
+#BSUB -q gpuv100                           # Queue to submit the job to
+#BSUB -W 800                             # Wall time limit (6 hours)
+#BSUB -n 4                                 # Request 4 cores
+#BSUB -R "rusage[mem=1GB]"                 # Request 1 GB of memory per core
+#BSUB -R "span[hosts=1]"                   # Request all cores on the same host
+#BSUB -gpu "num=1:mode=exclusive_process"  # Request 1 GPU in exclusive mode
+#BSUB -o src/outputs/vae-mnist.out                        # Standard output redirection
+#BSUB -e src/outputs/vae-mnist.err                        # Standard error redirection
+#BSUB -N                                   # send email when job finishes
+#BSUB -B                                   # Send email when job begins
+
+# Activate virtual environment
+source /zhome/66/4/156534/Master_Thesis/.venv/bin/activate
+
+# --- Phase 1+2+3: Training ---
+python /zhome/66/4/156534/Master_Thesis/src/scripts/VAE-Baseline-Training.py \
+    --run_name vae-mnist \
+    --dataset mnist \
+    --epochs 300 \
+    --batch_size 128 \
+    --lr 1e-4 \
+    --weight_decay 1e-5 \
+    --grad_clip 1.0 \
+    --subset_frac 1.0 \
+    --lambda_kl_max 0.01 \
+    --kl_warmup_frac 0.4 \
+    --latent_dim 32 \
+    --latent_size 4 \
+    --latent_patch_size 2 \
+    --latent_enc_hidden_dim 12 \
+    --dec_trans_dim 128 \
+    --dec_trans_n_head 8 \
+    --dec_trans_head_dim 32 \
+    --dec_trans_ff_dim 1024 \
+    --dec_trans_enc_depth 4 \
+    --dec_trans_dec_depth 4 \
+    --dec_trans_n_groups 32 \
+    --dec_trans_update_strategy scale \
+    --inr_hidden_dim 128 \
+    --inr_layers 3
