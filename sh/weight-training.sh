dc5fd65c88b61a5c4260edd818b8390dda4b27b9
Frederik12345678 <frederiknagel9@gmail.com>
2026-06-23 20:53:14 +0200

    add better training sh scripts

---
+++ sh/weight-training.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#BSUB -J weight-diffusion      # Job name
+#BSUB -q gpuv100                          # Queue to submit the job to
+#BSUB -W 1440                             # Wall time limit (6 hours)
+#BSUB -n 4                                 # Request 8 cores
+#BSUB -R "rusage[mem=2GB]"                 # Request 2 GB of memory per core
+#BSUB -R "span[hosts=1]"                   # Request all cores on the same host
+#BSUB -gpu "num=1:mode=exclusive_process"  # Request 1 GPU in exclusive mode
+#BSUB -o src/outputs/weight-diffusion.out                        # Standard output redirection
+#BSUB -e src/outputs/weight-diffusion.err                        # Standard error redirection
+#BSUB -N                                   # send email when job finishes
+#BSUB -B                                   # Send email when job begins
+
+# Activate virtual environment
+source /zhome/66/4/156534/Master_Thesis/.venv/bin/activate
+
+# --- Phase 1+2+3: Training ---
+python /zhome/66/4/156534/Master_Thesis/main.py\
+    --run_name weight-diffusion \
+    --epochs 450 \
+    --batch_size 128 \
+    --subset_frac 1.0 \
+    --stop_gradient_flow False \
+    --n_fid_samples 4096 \
+    --model weight_inr_diffusion\
+    --dataset mnist \
+    --lr 1e-4 \
+    --weight_decay 1e-5 \
+    --grad_clip 1.0 \
+    --log_every_n_steps 200 \
+    --peak_lr 1e-4 \
+    --T 1000 \
+    --beta_1 1e-4 \
+    --beta_T 2e-2 \
+    --sigma_tilde 1.0 \
+    --inr_hidden_dim 128 \
+    --inr_layers 3 \
+    --encoder_trans_dim 128 \
+    --encoder_trans_n_head 8 \
+    --encoder_trans_head_dim 32 \
+    --encoder_trans_ff_dim 1024 \
+    --encoder_trans_enc_depth 4 \
+    --encoder_trans_dec_depth 4 \
+    --encoder_trans_patch_size 2 \
+    --encoder_trans_n_groups 32 \
+    --encoder_trans_update_strategy scale \
+    --predictor_variant transformer \
+    --noise_predictor_dim 128 \
+    --noise_predictor_n_head 8 \
+    --noise_predictor_head_dim 32 \
+    --noise_predictor_ff_dim 1024 \
+    --noise_predictor_depth 6 \
+    --noise_predictor_dropout 0.1 \
+    --noise_predictor_chunk_size 16 \
+    --noise_predictor_t_embed_dim 128
